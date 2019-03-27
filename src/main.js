eb1e8ce244324793c9a4de722454df9756e91b3b
frjcxy <mound87@163.com>
2019-03-27 11:50:45 +0800

    index内容和路由整合

--- src/main.js
+++ src/main.js
@@ -1,9 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
-import style from './assets/site/css/style.css'
+// import style from './assets/site/css/style.css'
+import './assets/site/css/style.css'
+// 导入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//导入组件
+import index from './components/index.vue'
+//规则
+let routes=[
+  {
+    path:'/',
+    component:index
+  },
+  {
+    path:'/index',
+    component:index
+  }
+]
+
+let router=new VueRouter({
+  routes
+})
+
 
 Vue.config.productionTip = false
 
+
+
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
